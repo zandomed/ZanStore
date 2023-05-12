8464bd2e7b78d7161ce18cb584060d955bd1fbb5
Miguel Mendoza <miguel@zandome.com>
2023-05-11 23:36:21 -0400

    🗃️ Add Foreign Key User in Product Migration

--- database/migrations/2023_05_11_125631_create_products_table.php
+++ database/migrations/2023_05_11_125631_create_products_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
+            $table->foreignUuid('user_id');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
